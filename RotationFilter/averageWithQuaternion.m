6ae836e5c94030b7280387c6e8a2b2085c5d25ac
Tarun <tpunnoose@users.noreply.github.com>
2019-05-17 20:59:49 -0700

    Added in proper quaternion averaging

---
+++ RotationFilter/averageWithQuaternion.m
@@ -0,0 +1,20 @@
+function xbar = averageWithQuaternion(x, w)
+% based off method from:
+% Markley, F. Landis, et al. "Averaging quaternions." 
+% Journal of Guidance, Control, and Dynamics 30.4 (2007): 1193-1197.
+    n = numel(x(:,1));
+    
+    xbar = zeros(13,1);
+    
+    xbar([1:3 8:13]) = x([1:3 8:13], :)*w;
+    
+    Q = x(4:7, :)*diag(sqrt(w));
+    A = Q*Q';
+    
+    [Qbar, Lam] = eig(A);
+        
+    [~,index] = max(diag(Lam)); 
+    qbar = Qbar(:,index); 
+    
+    xbar(4:7) = qbar;
+end
