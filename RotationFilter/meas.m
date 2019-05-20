e1092145b07790ec6ea9bbe62024d126381bbddc
Tarun <tpunnoose@users.noreply.github.com>
2019-05-20 15:55:36 -0700

    Fixed Measurement Model

--- RotationFilter/meas.m
+++ RotationFilter/meas.m
@@ -3,6 +3,10 @@ function y = meas(x)
     % w_m = angular velocity measured by gyroscope
     y = zeros(6,1);
     
-    y(1:3) = 9.81;
+    q_g_w = [0 0 0 9.81]';
+    
+    q_g_b = quat_prod(x(4:7), quat_prod(q_g_w, x(4:7).*[1 -1 -1 -1]));
+    
+    y(1:3) = q_g_b(2:4);
     y(4:6) = x(11:13);
 end
